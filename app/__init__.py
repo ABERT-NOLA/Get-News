7e3187ab3629fb06a5ee478b0ed9ea602b93b9cb
Albert Obwoge <albertnyambegaa@gmail.com>
2020-07-23 01:21:05 +0300

    added get sources

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,28 @@
 from flask import Flask
-from app.config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap
-from app import views
-from app import error
+
 
 bootstrap = Bootstrap()
-
-   # Initializing application
-app = Flask(__name__,instance_relative_config = True)
-
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile("config.py")
-
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
-
-# register a blueprint
-from .main import main as main_blueprint
-app.register_blueprint(main_blueprint)
-# setup config
-from .requests import configure_request
-configure_request(app)
-
-def create_app():
+def create_app(config_name):
    app=Flask(__name__)
+
+      # Initializing application
+
+      # Setting up configuration
+   app.config.from_object(config_options[config_name])
+
+
+   # Initializing Flask Extensions
+   bootstrap.init_app(app)
+   # register a blueprint
+   from .main import main as main_blueprint
+   app.register_blueprint(main_blueprint)
+   # setup config
+   from .requests import configure_requests
+   configure_requests(app)
    return app
+
+
+
+
---
+++ app/requests.py
@@ -0,0 +1,82 @@
+#from app import app
+import urllib.request,json
+#from .main import main
+
+
+#News = news.News
+# Getting api key
+api_key = None
+# Getting the news base url
+base_url = None
+def configure_requests(app):
+    global api_key,base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config ['NEWS_API_BASE_URL']
+
+def get_news(category):
+
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_news_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['results']:
+            news_results_list = get_news_response['results']
+            news_results = process_results(news_results_list)
+
+
+    return news_results
+def get_sources(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(category, api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_sources(sources_results_list)
+
+    return sources_results
+def process_results(news_list):
+    
+    news_results = []
+    for news_item in news_list:
+        id = news_item.get('id')
+        title = news_item.get('original_title')
+        overview = news_item.get('overview')
+        poster = news_item.get('poster_path')
+        vote_average = news_item.get('vote_average')
+        vote_count = news_item.get('vote_count')
+
+    if poster:
+        news_object = news(id,title,overview,poster,vote_average,vote_count)
+        news_results.append(news_object)
+
+    return news_results
+
+def search_news(news_name):
+
+    search_news_url = 'https://newsapi.org/v2/top-headlines?sources=%s&apiKey=%s'.format(api_key,news_name)
+    with urllib.request.urlopen(search_news_url) as url:
+        search_news_data = url.read()
+        search_news_response = json.loads(search_news_data)
+
+    search_news_results = None
+
+    if search_news_response['results']:
+        search_news_list = search_news_response['results']
+        search_news_results = process_results(search_news_list)
+
+
+    return search_news_results
